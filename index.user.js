b976c3464903acde3024e0bc2a4d30c6de54d604
hinjin <a6051529@163.com>
2020-02-20 22:33:34 +0800

    Create index.user.js

---
+++ index.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         华为商城抢购插件
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.vmall.com/product/10086831441169.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(() => {
+        console.log(2222);
+        if ($('#pro-operation .product-button02').text() === '立即申购') {
+            rush.business.doGoRush(2);
+        }
+    }, 50);
+})();
